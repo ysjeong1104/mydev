d72398b111f2c2a064f467f4bd9901fd2779b297
ysjeong1104 <whseok@icloud.com>
2022-10-20 15:03:10 +0900

    first commit

---
+++ oracletest/oracletest/src/main/java/com/study/oracletest/OracletestApplication.java
@@ -0,0 +1,13 @@
+package com.study.oracletest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OracletestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OracletestApplication.class, args);
+	}
+
+}
